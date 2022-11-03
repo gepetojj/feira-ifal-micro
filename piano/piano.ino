81d289b5e9ff2ba47ea11877a83b35b64c08fb22
Letícia Duarte <leticiabsduarte@gmail.com>
2022-11-03 12:24:18 -0300

    pinos ordenados e uma escala acima

--- piano/piano.ino
+++ piano/piano.ino
@@ -15,108 +15,74 @@
 const int threshold = 20;
 int touchValue;
 
-void setup()
-{
-	Serial.begin(115200);
-	ledcAttachPin(BUZZER_PIN, BUZZER_CHANNEL);
-	delay(1000);
+void setup() {
+  Serial.begin(115200);
+  ledcAttachPin(BUZZER_PIN, BUZZER_CHANNEL);
+  delay(1000);
 }
 
-void loop()
-{
-	if (touchValue = touchRead(TOUCH_1_PIN))
-	{
-		if (touchValue < threshold)
-		{
-			tone(BUZZER_PIN, NOTE_C4, 100, BUZZER_CHANNEL);
-		}
-		else
-		{
-			noTone(BUZZER_PIN, BUZZER_CHANNEL);
-		}
-	}
+void loop() {
+  if (touchValue = touchRead(TOUCH_1_PIN)) {
+    if (touchValue < threshold) {
+      tone(BUZZER_PIN, NOTE_C6, 100, BUZZER_CHANNEL);
+    } else {
+      noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    }
+  }
 
-	if (touchValue = touchRead(TOUCH_2_PIN))
-	{
-		if (touchValue < threshold)
-		{
-			tone(BUZZER_PIN, NOTE_D4, 100, BUZZER_CHANNEL);
-		}
-		else
-		{
-			noTone(BUZZER_PIN, BUZZER_CHANNEL);
-		}
-	}
+  if (touchValue = touchRead(TOUCH_2_PIN)) {
+    if (touchValue < threshold) {
+      tone(BUZZER_PIN, NOTE_B5, 100, BUZZER_CHANNEL);
+    } else {
+      noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    }
+  }
 
-	if (touchValue = touchRead(TOUCH_3_PIN))
-	{
-		if (touchValue < threshold)
-		{
-			tone(BUZZER_PIN, NOTE_E4, 100, BUZZER_CHANNEL);
-		}
-		else
-		{
-			noTone(BUZZER_PIN, BUZZER_CHANNEL);
-		}
-	}
+  if (touchValue = touchRead(TOUCH_3_PIN)) {
+    if (touchValue < threshold) {
+      tone(BUZZER_PIN, NOTE_C5, 100, BUZZER_CHANNEL);
+    } else {
+      noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    }
+  }
 
-	if (touchValue = touchRead(TOUCH_4_PIN))
-	{
-		if (touchValue < threshold)
-		{
-			tone(BUZZER_PIN, NOTE_F4, 100, BUZZER_CHANNEL);
-		}
-		else
-		{
-			noTone(BUZZER_PIN, BUZZER_CHANNEL);
-		}
-	}
+  if (touchValue = touchRead(TOUCH_4_PIN)) {
+    if (touchValue < threshold) {
+      tone(BUZZER_PIN, NOTE_D5, 100, BUZZER_CHANNEL);
+    } else {
+      noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    }
+  }
 
-	if (touchValue = touchRead(TOUCH_5_PIN))
-	{
-		if (touchValue < threshold)
-		{
-			tone(BUZZER_PIN, NOTE_G4, 100, BUZZER_CHANNEL);
-		}
-		else
-		{
-			noTone(BUZZER_PIN, BUZZER_CHANNEL);
-		}
-	}
+  if (touchValue = touchRead(TOUCH_5_PIN)) {
+    if (touchValue < threshold) {
+      tone(BUZZER_PIN, NOTE_E5, 100, BUZZER_CHANNEL);
+    } else {
+      noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    }
+  }
 
-	if (touchValue = touchRead(TOUCH_6_PIN))
-	{
-		if (touchValue < threshold)
-		{
-			tone(BUZZER_PIN, NOTE_A4, 100, BUZZER_CHANNEL);
-		}
-		else
-		{
-			noTone(BUZZER_PIN, BUZZER_CHANNEL);
-		}
-	}
+  if (touchValue = touchRead(TOUCH_6_PIN)) {
+    if (touchValue < threshold) {
+      tone(BUZZER_PIN, NOTE_F5, 100, BUZZER_CHANNEL);
+    } else {
+      noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    }
+  }
 
-	if (touchValue = touchRead(TOUCH_7_PIN))
-	{
-		if (touchValue < threshold)
-		{
-			tone(BUZZER_PIN, NOTE_B4, 100, BUZZER_CHANNEL);
-		}
-		else
-		{
-			noTone(BUZZER_PIN, BUZZER_CHANNEL);
-		}
-	}
+  if (touchValue = touchRead(TOUCH_7_PIN)) {
+    if (touchValue < threshold) {
+      tone(BUZZER_PIN, NOTE_G5, 100, BUZZER_CHANNEL);
+    } else {
+      noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    }
+  }
 
-	if (touchValue = touchRead(TOUCH_8_PIN))
-	{
-		if (touchValue < threshold)
-		{
-			tone(BUZZER_PIN, NOTE_C5, 100, BUZZER_CHANNEL);
-		}
-		else
-		{
-			noTone(BUZZER_PIN, BUZZER_CHANNEL);
-		}
-	}
+  if (touchValue = touchRead(TOUCH_8_PIN)) {
+    if (touchValue < threshold) {
+      tone(BUZZER_PIN, NOTE_A5, 100, BUZZER_CHANNEL);
+    } else {
+      noTone(BUZZER_PIN, BUZZER_CHANNEL);
+    }
+  }
 }
